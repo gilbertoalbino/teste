23589ced766fa02bfc3c8d19732649d63f48063a
gilbertoalbino <contato@gilbertoalbino.com>
2017-09-12 15:27:22 -0300

    file src/../CandidateController.php added to project.

---
+++ src/Serasa/Controller/CandidateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Serasa\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class CandidateController controls the API Candidate CRUD actions.
+ * @package Serasa\Controller
+ */
+class CandidateController
+{
+
+}
