013fa092e7fece302771ee8ac9ef1747464e8b50
gilbertoalbino <contato@gilbertoalbino.com>
2017-09-12 15:42:45 -0300

    Creating a candidate action added to API.

--- src/Serasa/Controller/CandidateController.php
+++ src/Serasa/Controller/CandidateController.php
@@ -63,4 +63,75 @@ class CandidateController
         ], 200);
     }
 
+
+    /**
+     * Creates a new Candidate.
+     *
+     * @param Application $app
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Application $app, Request $request)
+    {
+        $message = 'Creating Candidate';
+
+        /**
+         * Validates if any data inconsistence is found.
+         */
+        $validatorErrors = CandidateValidator::factory(
+            $request->query->all(),
+            CandidateValidator::IS_INSERT
+        )->getErrors();
+
+        /**
+         * Application will stop and return an array of errors
+         * matched by key => message to be used in error decoration.
+         */
+        if (!empty($validatorErrors)) {
+            return new JsonResponse(['errors' => $validatorErrors], 400);
+        }
+
+        /**
+         * Cleans up any dirty and removes not fillable columns.
+         */
+        $data = CandidateSanitizer::factory($request->query->all());
+
+        /**
+         * Checks if the candidate is already registered.
+         */
+        $sql = sprintf("
+              SELECT * FROM candidates 
+              WHERE fullname = :fullname 
+              AND birthdate = :birthdate"
+        );
+
+        $statement = $app['db']->prepare($sql);
+        $statement->bindValue('fullname', $data->fullname);
+        $statement->bindValue('birthdate', $data->birthdate);
+        $statement->execute();
+
+        $candidate = $statement->fetch();
+
+        if ($candidate) {
+            return new JsonResponse([
+                'message' => $message,
+                'error' => 'Candidate is already registered.'
+            ], 400);
+        }
+
+        /**
+         * The candidate is created just with a name and a birthdate.
+         * Scores values must be provided by update action.
+         */
+        $candidate = $app['db']->insert('candidates', [
+            'fullname' => $data->fullname,
+            'birthdate' => $data->birthdate
+        ]);
+
+        return new JsonResponse([
+            'message' => $message,
+            'candidate' => $candidate
+        ], 200);
+    }
+
 }
