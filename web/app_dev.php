b07f732f5e43154af0dc5f960317d387b9faa480
gilbertoalbino <contato@gilbertoalbino.com>
2017-09-12 15:02:31 -0300

    web entry files added to project.

---
+++ web/app_dev.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zyne
+ * Date: 12/09/2017
+ * Time: 15:00
+ */
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * This file is used as a proxy to Nginx and PHP Built-in server.
+ */
+include 'app.php';
