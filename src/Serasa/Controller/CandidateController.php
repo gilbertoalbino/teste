1f20c796869ded81118c3bab605425fe6fbfd6ae
gilbertoalbino <contato@gilbertoalbino.com>
2017-09-12 15:32:11 -0300

    Listing all candidates action added to API.

--- src/Serasa/Controller/CandidateController.php
+++ src/Serasa/Controller/CandidateController.php
@@ -12,5 +12,27 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class CandidateController
 {
+    /**
+     * Lists all candidates summing up the scores.
+     *
+     * @param Application $app
+     * @return JsonResponse
+     */
+    public function index(Application $app)
+    {
+        $message = 'Listing Candidates';
 
+        $sql = "SELECT * FROM candidates";
+
+        $statement = $app['db']->prepare($sql);
+        $statement->execute();
+        $statement->setFetchMode(\PDO::FETCH_CLASS, CandidateHydrator::class);
+
+        $candidates = $statement->fetchAll();
+
+        return new JsonResponse([
+            'message' => $message,
+            'candidates' => $candidates
+        ], 200);
+    } 
 }
